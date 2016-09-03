aeb3df3b824f3cb2790f44c8af8365cae7b612eb
Jeff Sonstein <jsonstein@gmail.com>
2016-09-03 19:05:27 -0400

    Death to the Fascist Running dog Globals!
    Onward the Refactoring!

--- tsv2json.d
+++ tsv2json.d
@@ -2,19 +2,25 @@ import std.string;
 import std.stdio;
 import std.array;
 
-string[] myHeaders; // at this scope so we can reuse in output
-
-string oneRecord( string[] someTokens ) {
+string oneRecord( string[] someTokens, string[] someHeaders ) {
   string thisRecord = "{";
   string foo;
   auto thisLength = someTokens.length;
   for( int i=0; i<(thisLength-1); i++ ) {
     foo = replace( someTokens[ i ], "\"", " " );
-    thisRecord = thisRecord ~ "\"" ~ myHeaders[i] ~ "\": \"" ~ foo ~ "\",";
+    thisRecord = thisRecord ~ "\"" ~ someHeaders[i] ~ "\": \"" ~ foo ~ "\",";
   }
   foo = replace( someTokens[ thisLength-1 ], "\"", " " );
-  thisRecord = thisRecord ~ "\"" ~ myHeaders[thisLength-1] ~ "\": \"" ~ foo ~ "\"}";
+  thisRecord = thisRecord ~ "\"" ~ someHeaders[thisLength-1] ~ "\": \"" ~ foo ~ "\"}";
   return thisRecord;
+}
+
+string[] cleanHeaders( string aLine ) {
+  string[] theseHeaders = aLine.split( "\t" );
+  for( int i=0; i<theseHeaders.length; i++ ) {
+    theseHeaders[ i ] = replace( theseHeaders[ i ], "\"", " " );
+  }
+  return theseHeaders;
 }
 
 void main( string[] args ) {
@@ -25,7 +31,7 @@ void main( string[] args ) {
   string line;
   // get the headers
   readf(" %s\n ", &line);
-  myHeaders = line.split( "\t" );
+  string[] myHeaders = cleanHeaders( line );
   writef( "[" ); // start array
   bool firstOne = true;
   while ( (readf(" %s\n ", &line)) >= 1 ) { // at least 1 char returned
@@ -33,7 +39,7 @@ void main( string[] args ) {
       writef( "," );
     }
     firstOne = false;
-    writef( "%s", oneRecord( line.split( "\t" ) ) );
+    writef( "%s", oneRecord( line.split( "\t" ), myHeaders ) );
   }
   writef( "]" );
 }
