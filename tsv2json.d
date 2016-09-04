53b775872a3502c909d4c9b21c1681155c9a8323
Jeff Sonstein <jsonstein@gmail.com>
2016-09-04 09:54:30 -0400

    better sanitization of inputs
    quotes become spaces in data, spaces become underscores in headers,
    generally cleaning it up a bit

--- tsv2json.d
+++ tsv2json.d
@@ -1,17 +1,18 @@
 import std.string;
+import std.regex;
 import std.stdio;
 import std.array;
 
 string oneRecord( string[] someTokens, string[] someHeaders ) {
   string thisRecord = "{";
-  string foo;
+  string thisData;
   auto thisLength = someTokens.length;
   for( int i=0; i<(thisLength-1); i++ ) {
-    foo = replace( someTokens[ i ], "\"", " " );
-    thisRecord = thisRecord ~ "\"" ~ someHeaders[i] ~ "\": \"" ~ foo ~ "\",";
+    thisData = replace( someTokens[ i ], "\"", " " );
+    thisRecord = thisRecord ~ "\"" ~ someHeaders[i] ~ "\": \"" ~ thisData ~ "\",";
   }
-  foo = replace( someTokens[ thisLength-1 ], "\"", " " );
-  thisRecord = thisRecord ~ "\"" ~ someHeaders[thisLength-1] ~ "\": \"" ~ foo ~ "\"}";
+  thisData = replace( someTokens[ thisLength-1 ], "\"", " " );
+  thisRecord = thisRecord ~ "\"" ~ someHeaders[thisLength-1] ~ "\": \"" ~ thisData ~ "\"}";
   return thisRecord;
 }
 
@@ -19,27 +20,31 @@ string[] cleanHeaders( string aLine ) {
   string[] theseHeaders = aLine.split( "\t" );
   for( int i=0; i<theseHeaders.length; i++ ) {
     theseHeaders[ i ] = replace( theseHeaders[ i ], "\"", " " );
+    theseHeaders[ i ] = replace( theseHeaders[ i ], " ", "_" );
   }
   return theseHeaders;
 }
 
 void main( string[] args ) {
   if ( args.length > 1 ) {
-    stderr.writef( "%s", "Usage: tsv2json\n\nreads a tsv file from stdin and writes a json file to stdout" );
-    stderr.writef( "%s", "at the moment it just assumes there is a header record with reasonable JSON keys" );
+    stderr.writeln( "Usage: tsv2json" );
+    stderr.writeln( "       reads a tsv file from stdin and writes a json file to stdout" );
+    stderr.writeln( "       at the moment it just assumes there is a header record to use as JSON keys" );
   }
-  string line;
-  // get the headers
-  readf(" %s\n ", &line);
-  string[] myHeaders = cleanHeaders( line );
-  writef( "[" ); // start array
-  bool firstOne = true;
-  while ( (readf(" %s\n ", &line)) >= 1 ) { // at least 1 char returned
-    if( !firstOne ) {
-      writef( "," );
+  else {
+    string line;
+    // get the headers
+    readf(" %s\n ", &line);
+    string[] myHeaders = cleanHeaders( line );
+    writef( "[" ); // start array
+    bool firstOne = true;
+    while ( (readf(" %s\n ", &line)) >= 1 ) { // at least 1 char returned
+      if( !firstOne ) {
+        writef( "," );
+      }
+      writef( "%s", oneRecord( line.split( "\t" ), myHeaders ) );
+      firstOne = false;
     }
-    firstOne = false;
-    writef( "%s", oneRecord( line.split( "\t" ), myHeaders ) );
+    writef( "]" );
   }
-  writef( "]" );
 }
